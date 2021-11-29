7fb15c37cfbfecace8b95269c5926aa531dbc6a8
Tejas <>
2021-11-29 17:11:11 +0530

    Dark Mode added to home page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 import { Provider as AuthProvider } from "next-auth/client";
-import { useEffect } from "react";
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import { store } from "../app/store";
-import { selectItems } from "../slices/basketSlice";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }) => {
